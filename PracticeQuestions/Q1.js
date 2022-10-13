09696859cce64d348215d8b685db314405ba1b29
sairajthorat <thoratsairaj10@gmail.com>
2022-10-13 23:29:47 +0530

    basic js practice questions

---
+++ PracticeQuestions/Q1.js
@@ -0,0 +1,5 @@
+// Q1 create a variable of type string and try to add a number to it 
+
+let a = "sairaj"
+let b = 5
+console.log(a + b)
---
+++ PracticeQuestions/Q5.js
@@ -0,0 +1,12 @@
+// Q5 write a JS program to create a word-meanimg dict. of 5 words.
+
+const dict = {
+    a:"b",
+    x:"y",
+    p:"q",
+    s:"t",
+    j:"k",
+
+}
+
+console.log(dict[a]) //also written as console.log(dict.a)
